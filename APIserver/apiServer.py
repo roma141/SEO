592f916589a207c9ea96c879e3ff83c6575ff962
Andres Botello <roma141@gmail.com>
2016-10-14 20:59:36 -0500

    part 1 completed

--- APIserver/apiServer.py
+++ APIserver/apiServer.py
@@ -1,8 +1,20 @@
+from apiDB import DB
 
-def save_positions(positions):
-    for a in positions:
-        print a["title"]
+def save_positions(query, positions):
+    bd = DB()
+    for a in positions:        
+        bd.Ejecuta("""insert into positions (query, position, title, url, description, date) values("%s", %s,"%s","%s","%s", UTC_TIMESTAMP())""" 
+                       % (query.encode("utf-8"), a["position"], a["title"].encode("utf-8"), a["url"].encode("utf-8"), a["description"].encode("utf-8")))
+    bd.cierra()
     
 def save_suggested(query,suggested):
+    bd = DB()
     for a in suggested:
-        print a
+        bd.Ejecuta("""insert into suggesteds (query, position, suggested, date) values("%s",%s,"%s", UTC_TIMESTAMP())""" 
+                       % (query.encode("utf-8"), a["position"], a["suggested"].encode("utf-8")))
+    bd.cierra()
+
+def get_query():
+    bd = DB()
+    querys = bd.Ejecuta("select query from querys where state=1")
+    return querys[0]["query"].encode("utf-8")
--- google-query.py
+++ google-query.py
@@ -6,10 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from APIserver import apiServer
-query = "cheese!"
+# query = "cheese!"
+query = apiServer.get_query()
 browser = webdriver.Chrome()
 browser.get('http://google.com/')
-# print browser.title
 inputElement = browser.find_element_by_name("q")
 inputElement.send_keys(query)
 WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "sbqs_c")))
@@ -19,10 +19,13 @@ try:
     suggested = []
     positions = []
     WebDriverWait(browser, 10).until(EC.title_contains(query))
-#     print "page title = ", browser.title
+    c = 1
     for a in suggestedSearch:
-        #print a.text
-        suggested.append(a.text)
+        element = {}
+        element["position"] = c
+        element["suggested"] = a.text
+        suggested.append(element)
+        c += 1
     WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "g")))
     googleSearch = browser.find_elements_by_class_name("g")
     c = 1
@@ -30,11 +33,6 @@ try:
         element = {}
         if c > 10:
             break
-        #print "position = ", c
-        #print a.text
-        #print "title:  ", a.find_element_by_tag_name("h3").text
-        #print "url:  ", a.find_element_by_tag_name("cite").text
-        #print "description:  ", a.find_element_by_css_selector("span.st").text
         element["position"] = c
         element["title"] = a.find_element_by_tag_name("h3").text
         element ["url"] = a.find_element_by_tag_name("cite").text
@@ -42,6 +40,6 @@ try:
         positions.append(element)
         c += 1
 finally:
-    apiServer.save_positions(positions)
-    apiServer.save_suggested(query,suggested)
+    apiServer.save_positions(query, positions)
+    apiServer.save_suggested(query, suggested)
     browser.quit()
